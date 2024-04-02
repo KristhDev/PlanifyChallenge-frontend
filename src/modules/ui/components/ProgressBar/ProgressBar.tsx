b839b507c8222f154fbc275101f6678a4f9fea69
KristhDev <kristhdev@gmail.com>
2024-04-02 16:01:40 -0600

    Feat(ui): add ProgressBar component

---
+++ src/modules/ui/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,18 @@
+import { useSteps } from '../../../shared';
+
+export const ProgressBar = (): JSX.Element => {
+    const { currentStep } = useSteps();
+
+    return (
+        <div className="flex flex-col">
+            <p className="text-slate-800 text-lg mb-2">{ currentStep.title }</p>
+
+            <div className="bg-neutral-400 rounded-lg h-8 w-full">
+                <div 
+                    className="bg-indigo-600 rounded-lg h-8 duration-300"
+                    style={{ width: (currentStep.number / 4) * 100 + '%' }} 
+                />
+            </div>
+        </div>
+    );
+}
