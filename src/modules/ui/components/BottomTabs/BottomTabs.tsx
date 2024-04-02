24877f5c03105aa7a3f9abb38740d2c19a024991
KristhDev <kristhdev@gmail.com>
2024-04-02 16:36:08 -0600

    Feat(ui): add conditional styles in BottomTabs component

--- src/modules/ui/components/BottomTabs/BottomTabs.tsx
+++ src/modules/ui/components/BottomTabs/BottomTabs.tsx
@@ -1,28 +1,39 @@
+import { NavLink } from 'react-router-dom';
 import { IoCalendarNumber, IoCalendar } from 'react-icons/io5';
+import clsx from 'clsx';
+
+const links = [
+    {
+        label: 'Reservar',
+        icon: <IoCalendarNumber size={ 24 } />,
+        to: '/'
+    },
+    {
+        label: 'Mis turnos',
+        icon: <IoCalendar size={ 24 } />,
+        to: '/my-appointments'
+    },
+];
 
 export const BottomTabs = (): JSX.Element => {
     return (
         <nav className="bg-white">
             <ul className="flex items-center justify-around gap-4">
-                <li>
-                    <a 
-                        href="#"
-                        className="bg-neutral-100 py-2 px-4 pb-1 text-indigo-600 border-b-4 border-indigo-600 flex flex-col items-center justify-center gap-1"
-                    >
-                        <IoCalendarNumber size={ 24 } />
-                        <span>Reservar</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a 
-                        href="#"
-                        className="py-2 px-4 pb-1 text-slate-900 border-b-4 border-slate-900 flex flex-col items-center justify-center gap-1"
-                    >
-                        <IoCalendar size={ 24 } />
-                        <span>Mis turnos</span>
-                    </a>
-                </li>
+                { links.map((link, index) => (
+                    <li key={ `${ link.label }-${ index }` }>
+                        <NavLink
+                            className={ ({ isActive }) => clsx(
+                                'py-2 px-4 pb-1 border-b-4 flex flex-col items-center justify-center gap-1',
+                                { 'bg-neutral-100 text-indigo-600 border-indigo-600': isActive },
+                                { 'text-slate-800 border-slate-800': !isActive }
+                            ) }
+                            to={ link.to }
+                        >
+                            { link.icon }
+                            <span>{ link.label }</span>
+                        </NavLink>
+                    </li>
+                )) }
             </ul>
         </nav>
     );
