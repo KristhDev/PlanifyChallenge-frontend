7caa1f808a8fbd300767a0543c80b91b0a4bf631
KristhDev <kristhdev@gmail.com>
2024-04-02 16:28:41 -0600

    Feat(router): use MainLayout in root router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 /* Modules */
 import { bookingsRoutes } from '../modules/bookings';
+import { MainLayout } from '../modules/ui';
 
 export const rootRouter = createBrowserRouter([
     {
         path: '',
+        element: (<MainLayout />),
         children: bookingsRoutes
     }
 ]);
