a76617e336c624bd0b2b17198d57f2fcea44c2d2
KristhDev <kristhdev@gmail.com>
2024-04-02 16:27:58 -0600

    Refactor(ui): fix MainLayout

--- src/modules/ui/layouts/MainLayout/MainLayout.tsx
+++ src/modules/ui/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,12 @@
-import { FC, PropsWithChildren } from 'react';
-
 /* Components */
 import { BottomTabs } from '../../components';
+import { Outlet } from 'react-router-dom';
 
-
-const MainLayout: FC<PropsWithChildren> = ({ children }): JSX.Element => {
+const MainLayout = (): JSX.Element => {
     return (
         <main className="dark:bg-neutral-200 h-svh flex flex-col justify-between">
             <div className="h-full">
-                { children }
+                <Outlet />
             </div>
 
             <BottomTabs />
