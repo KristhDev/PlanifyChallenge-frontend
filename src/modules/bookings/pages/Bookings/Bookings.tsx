9c52f2620468ca7024cf80ec5af008f9456f6175
KristhDev <kristhdev@gmail.com>
2024-04-03 07:07:52 -0600

    Feat(bookings): add components in Bookings page

--- src/modules/bookings/pages/Bookings/Bookings.tsx
+++ src/modules/bookings/pages/Bookings/Bookings.tsx
@@ -1,7 +1,17 @@
+import { ProgressBar } from '../../../ui';
+import { CategoryAcordion } from '../../components';
+
 const Bookings = (): JSX.Element => {
     return (
-        <div>
-            
+        <div className="flex flex-col h-max">
+            <ProgressBar />
+
+            <div className="bg-white border-[1px] border-gray-300 p-2 flex flex-col rounded-2xl shadow-md">
+                <CategoryAcordion />
+                <CategoryAcordion />
+                <CategoryAcordion />
+                <CategoryAcordion />
+            </div>
         </div>
     );
 }
