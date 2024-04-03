d7ec782e44e13780ef9efc7690d8bff8f552014a
KristhDev <kristhdev@gmail.com>
2024-04-03 12:11:25 -0600

    Feat(bookings): add funcionality for navigate to next page

--- src/modules/bookings/pages/Bookings/Bookings.tsx
+++ src/modules/bookings/pages/Bookings/Bookings.tsx
@@ -1,14 +1,26 @@
-import { ProgressBar, Spinner } from '../../../ui';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, ProgressBar, Spinner } from '../../../ui';
 import { CategoryAcordion } from '../../components';
 
 import { useBookings } from '../../hooks';
+import { useSteps } from '../../../shared';
 
 const Bookings = (): JSX.Element => {
-    const { categories, isCategoriesLoading } = useBookings();
+    const navigate = useNavigate();
+
+    const { categories, isCategoriesLoading, selectedService } = useBookings();
+    const { setCurrentStep } = useSteps();
+    const hasSelectedService = !!selectedService;
+
+    const handleNext = (): void => {
+        navigate('/bookings/time-slots');
+        setCurrentStep(2);
+    }
 
     return (
-        <div className="flex justify-center h-max">
-            <div className="flex flex-col lg:w-7/12 xl:w-6/12 2xl:w-5/12">
+        <div className="flex justify-center min-h-[calc(100svh_-_120px)]">
+            <div className="flex flex-col lg:w-7/12 xl:w-6/12 2xl:w-5/12 min-h-[inherit]">
                 <ProgressBar />
 
                 { (isCategoriesLoading) ? (
@@ -23,6 +35,17 @@ const Bookings = (): JSX.Element => {
                                 key={ `${ category.name }-${ index }` } 
                             />  
                         )) }
+                    </div>
+                ) }
+
+                { hasSelectedService && (
+                    <div className="flex flex-1 justify-end items-end pt-10">
+                        <Button 
+                            className="text-lg"
+                            onClick={ handleNext }
+                        >
+                            Siguiente
+                        </Button>
                     </div>
                 ) }
             </div>
