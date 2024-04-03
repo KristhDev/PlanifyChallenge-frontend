fac5f858f56307c90ee6dca296834f5a9db86e2e
KristhDev <kristhdev@gmail.com>
2024-04-03 07:01:31 -0600

    Style(ui): fix styles of MainLayout

--- src/modules/ui/layouts/MainLayout/MainLayout.tsx
+++ src/modules/ui/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 /* Components */
 import { BottomTabs } from '../../components';
-import { Outlet } from 'react-router-dom';
 
 const MainLayout = (): JSX.Element => {
     return (
-        <main className="dark:bg-neutral-200 h-svh flex flex-col justify-between">
-            <div className="h-full">
+        <main className="relative bg-neutral-200 min-h-svh flex flex-col justify-between">
+            <div className="flex-1 p-6 pb-32 bg-neutral-200">
                 <Outlet />
             </div>
 
