512c7ad63d22d57b7475198fbc901be535b4a9a1
KristhDev <kristhdev@gmail.com>
2024-04-03 07:02:20 -0600

    Style(ui): fix styles of BottomTabs

--- src/modules/ui/components/BottomTabs/BottomTabs.tsx
+++ src/modules/ui/components/BottomTabs/BottomTabs.tsx
@@ -17,15 +17,15 @@ const links = [
 
 export const BottomTabs = (): JSX.Element => {
     return (
-        <nav className="bg-white">
+        <nav className="bg-white w-full fixed bottom-0 left-0">
             <ul className="flex items-center justify-around gap-4">
                 { links.map((link, index) => (
                     <li key={ `${ link.label }-${ index }` }>
                         <NavLink
                             className={ ({ isActive }) => clsx(
                                 'py-2 px-4 pb-1 border-b-4 flex flex-col items-center justify-center gap-1',
-                                { 'bg-neutral-100 text-indigo-600 border-indigo-600': isActive },
-                                { 'text-slate-800 border-slate-800': !isActive }
+                                { 'bg-neutral-100 text-violet-600 border-violet-600': isActive },
+                                { 'text-neutral-800 border-neutral-800': !isActive }
                             ) }
                             to={ link.to }
                         >
