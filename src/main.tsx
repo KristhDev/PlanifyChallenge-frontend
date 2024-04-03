8ec7a46c3ae6827b90516ebe3b8764b963ecd127
KristhDev <kristhdev@gmail.com>
2024-04-03 15:36:19 -0600

    Feat: add locale for date util in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import 'dayjs/locale/es';
+
+import { date } from './utils';
 
 import './index.css';
+
+date.locale('es');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
