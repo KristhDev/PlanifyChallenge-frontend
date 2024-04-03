34d9a3a6dc0d87d65fcfee8e61d50ce0e7d67208
KristhDev <kristhdev@gmail.com>
2024-04-03 12:14:58 -0600

    Style(ui): fix styles of MainLayout

--- src/modules/ui/layouts/MainLayout/MainLayout.tsx
+++ src/modules/ui/layouts/MainLayout/MainLayout.tsx
@@ -6,7 +6,7 @@ import { BottomTabs } from '../../components';
 const MainLayout = (): JSX.Element => {
     return (
         <main className="relative bg-neutral-200 min-h-svh flex flex-col justify-between">
-            <div className="flex-1 p-6 pb-32 bg-neutral-200">
+            <div className="flex-1 p-6 pb-24 bg-neutral-200">
                 <Outlet />
             </div>
 
