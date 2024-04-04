48db0879c459a984dacdaafbcade6110639471d4
KristhDev <kristhdev@gmail.com>
2024-04-03 18:20:15 -0600

    Refactor(bookings): add more class to styles in TimeSlotSelector

--- src/modules/bookings/components/TimeSlotSelector/TimeSlotSelector.tsx
+++ src/modules/bookings/components/TimeSlotSelector/TimeSlotSelector.tsx
@@ -17,7 +17,7 @@ export const TimeSlotSelector: FC<TimeSlotSelectorProps> = ({ timeSlot }): JSX.E
 
     return (
         <div className="mb-4 last:mb-0">
-            <h2 className="text-lg text-neutral-800 mb-4">{ date.format(timeSlot.date, 'DD [de] MMMM') }</h2>
+            <h2 className="text-lg text-neutral-800 font-bold mb-4">{ date.format(timeSlot.date, 'DD [de] MMMM') }</h2>
 
             <div className="grid grid-cols-2 gap-4">
                 { timeSlot.availableTimeslots?.map((time, index) => (
