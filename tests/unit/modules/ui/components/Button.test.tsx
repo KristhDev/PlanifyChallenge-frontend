f8fad40f8fbbc7d0fde0c6acf54dfd56026b2ea5
KristhDev <kristhdev@gmail.com>
2024-04-03 21:26:44 -0600

    Test: add test in Button component

---
+++ tests/unit/modules/ui/components/Button.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+
+import { Button } from '../../../../../src/modules/ui';
+
+const renderComponent = () => render(
+    <Button className="bg-red-500">
+        Test
+    </Button>
+);
+
+describe('Test in <Button /> component', () => {
+    it('should to match snapshot', () => {
+        const { container } = renderComponent();
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should render props', () => {
+        renderComponent();
+
+        const btn = screen.queryByRole<HTMLButtonElement>('button')
+
+        expect(btn?.childNodes[0].textContent).toBe('Test');
+        expect(btn?.className).toContain('bg-red-500');
+    });
+});
