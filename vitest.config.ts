c756d092840cf7a17e9aefb202239ce1526c9b5e
KristhDev <kristhdev@gmail.com>
2024-04-03 21:26:17 -0600

    Feat(test): add test enviroment

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import vitestConfig from './vitest.config';
+
+export default mergeConfig(vitestConfig, defineConfig({
+    test: {
+        bail: 1,
+        coverage: {
+            provider: 'v8',
+            reporter: [ 'text', 'json', 'html' ],
+            reportsDirectory: './tests/coverage',
+        },
+        environment: 'jsdom',
+        globals: true
+    }
+}));
