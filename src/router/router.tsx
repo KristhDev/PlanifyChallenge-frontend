e9db447a33269291a9c0229098d4fa7d9ac6733f
KristhDev <kristhdev@gmail.com>
2024-04-03 18:33:27 -0600

    Feat(router): add MyShifts page in router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 /* Modules */
-import { bookingsRoutes } from '../modules/bookings';
+import { bookingsRoutes, MyShifts } from '../modules/bookings';
 import { MainLayout } from '../modules/ui';
 
 export const rootRouter = createBrowserRouter([
@@ -9,6 +9,13 @@ export const rootRouter = createBrowserRouter([
         path: 'bookings',
         element: (<MainLayout />),
         children: bookingsRoutes
+    },
+    {
+        path: 'my-shifts',
+        element: (<MainLayout />),
+        children: [
+            { path: '', element: (<MyShifts />) },
+        ]
     },
     {
         path: '*',
