d2e1147524e24d2ae1c887b28160ef144cbf9c3a
KristhDev <kristhdev@gmail.com>
2024-04-04 11:56:26 -0600

    Test(mocks): add mocks for tests

---
+++ tests/mocks/shared.ts
@@ -0,0 +1,9 @@
+import { Step } from '../../src/modules/shared';
+
+export const stepsMock: Step[] = [
+    { number: 1, title: 'Step 1' },
+    { number: 2, title: 'Step 2' },
+    { number: 3, title: 'Step 3' }
+];
+
+export const currentStepMock: Step = stepsMock[0];
